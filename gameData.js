3f51f2fa80b99b44327cc9c9f8d9aba765a9ee28
Suharsh Nagasampagi <suharsh329@gmail.com>
2025-06-11 15:04:56 +1000

    Update questions

--- gameData.js
+++ gameData.js
@@ -3,45 +3,42 @@ const defaultGameData = {
   boards: [
     {
       categories: [
-        "Better known by one name",
-        "It is slang-tastic",
-        "Brand Names",
-        "V-ocabulary",
-        "Literary Significant Others",
+        "HOMOPHONIC PAIRS",
+        "MY 'MAN'!",
+        "CHECK YOUR CALENDAR",
+        "A CONTAINER FULL OF WORDS",
+        "GET YOUR GROUP ON",
       ],
       questions: [
         [
           {
             points: 200,
-            question: "This Queen of Pop was born Madonna Louise Ciccone",
-            answer: "Who is Madonna?",
+            question: "A story about a beastly appendage",
+            answer: "What is a tail tale?",
             revealed: false,
           },
           {
             points: 400,
-            question:
-              "Born Stefani Joanne Angelina Germanotta, this artist is known for hits like 'Bad Romance'",
-            answer: "Who is Lady Gaga?",
+            question: "To wander through the land of the Caesars",
+            answer: "What is roam Rome?",
             revealed: false,
           },
           {
             points: 600,
-            question:
-              "This 'Material Girl' singer's real name is Madonna Louise Ciccone",
-            answer: "Who is Madonna?",
+            question: "The right moment for a pungent herb of the mint family",
+            answer: "What is thyme time?",
             revealed: false,
           },
           {
             points: 800,
-            question:
-              "Born Eric Arthur Blair, this author wrote '1984' and 'Animal Farm'",
-            answer: "Who is George Orwell?",
+            question: "The 60 minutes that belong to you & me",
+            answer: "What is our hour?",
             revealed: false,
           },
           {
             points: 1000,
-            question: "This boxing legend was born Cassius Marcellus Clay Jr.",
-            answer: "Who is Muhammad Ali?",
+            question: "A scratchy & hard-to-wear elaborate starched collar",
+            answer: "What is a rough ruff?",
             revealed: false,
           },
         ],
@@ -49,32 +46,36 @@ const defaultGameData = {
         [
           {
             points: 200,
-            question: "This word means 'excellent' and rhymes with 'lit'",
-            answer: "What is 'sick'?",
+            question: "It's a 3-D representation of human form, dummy",
+            answer: "What is a mannequin?",
             revealed: false,
           },
           {
             points: 400,
-            question: "When something is 'fire,' it means this",
-            answer: "What is awesome or amazing?",
+            question: "Hand shackles",
+            answer: "What are manacles?",
             revealed: false,
           },
           {
             points: 600,
-            question: "To 'ghost' someone means to do this to them",
-            answer: "What is ignore them or cut off contact?",
+            question:
+              "It's your destiny to know this 8-letter word means 'to show plainly'",
+            answer: "What is manifest?",
             revealed: false,
           },
           {
             points: 800,
-            question: "This 3-letter abbreviation means 'laughing out loud'",
-            answer: "What is LOL?",
+            question:
+              "A royal command, or one from a superior court to a lower one",
+            answer: "What is a mandate?",
             revealed: false,
           },
+
           {
             points: 1000,
-            question: "When someone is 'sus,' they are this",
-            answer: "What is suspicious?",
+            question:
+              "It conveys air & fuel from the carburetor to the cylinders",
+            answer: "What is a manifold?",
             revealed: false,
           },
         ],
@@ -82,33 +83,74 @@ const defaultGameData = {
         [
           {
             points: 200,
-            question: "This swoosh logo belongs to this athletic brand",
-            answer: "What is Nike?",
+            question:
+              "Colorful nickname for November 27, 2020, knowing that Thanksgiving is November 26",
+            answer: "What is Black Friday?",
             revealed: false,
           },
           {
             points: 400,
-            question: "This coffee chain's logo features a green mermaid",
-            answer: "What is Starbucks?",
+            question:
+              "You are this sign of the zodiac if your birthday falls between December 22 & January 19",
+            answer: "What is a Capricorn?",
             revealed: false,
           },
           {
             points: 600,
-            question: "This tech company's slogan is 'Think Different'",
-            answer: "What is Apple?",
+            question:
+              "Bastille Day, Parents' Day & Nelson Mandela Day are all observed during this month",
+            answer: "What is July?",
             revealed: false,
           },
           {
             points: 800,
-            question: "This fast-food chain is 'lovin' it'",
-            answer: "What is McDonald's?",
+            question:
+              "Place your bets--this sporting event established in 1875 takes place the first Saturday in May",
+            answer: "What is the Kentucky Derby?",
             revealed: false,
           },
           {
             points: 1000,
             question:
-              "This luxury car brand's logo features four interlocking rings",
-            answer: "What is Audi?",
+              "Commemorating an event from 1777, June 14 is this holiday in the U.S.",
+            answer: "What is Flag Day?",
+            revealed: false,
+          },
+        ],
+        [
+          {
+            points: 200,
+            question:
+              "A woven container, or the structure under a hot air balloon for the crew & equipment",
+            answer: "What is a basket?",
+            revealed: false,
+          },
+          {
+            points: 400,
+            question:
+              "A handbag; it can also refer to the prize money in a boxing match",
+            answer: "What is a purse?",
+            revealed: false,
+          },
+          {
+            points: 600,
+            question:
+              "Wearable term for the box from which cards are dealt for blackjack in a casino",
+            answer: "What is a shoe?",
+            revealed: false,
+          },
+          {
+            points: 800,
+            question:
+              "This small glass container can begin with 'v' or with 'ph'",
+            answer: "What is a vial(phial)?",
+            revealed: false,
+          },
+          {
+            points: 1000,
+            question:
+              "From the Latin for 'box', it's an underground reservoir for collecting rainwater",
+            answer: "What is a cistern?",
             revealed: false,
           },
         ],
@@ -116,68 +158,32 @@ const defaultGameData = {
         [
           {
             points: 200,
-            question: "This word means having a lot of energy and enthusiasm",
-            answer: "What is vivacious?",
+            question: "This group of 7 includes envy, pride & sloth",
+            answer: "What are the 7 deadly sins?",
             revealed: false,
           },
           {
             points: 400,
-            question: "This type of poem has 14 lines and often expresses love",
-            answer:
-              "What is a sonnet? (Wait, that doesn't start with V... What is verse?)",
+            question: "Albania & Kosovo are among this group of countries that cover a peninsula in Europe",
+            answer: "What is the Balkans?",
             revealed: false,
           },
           {
             points: 600,
-            question: "This word means to disappear suddenly",
-            answer: "What is vanish?",
+            question: "TThe North Star is part of this famous grouping within Ursa Minor",
+            answer: "What is the Little Dipper?",
             revealed: false,
           },
           {
             points: 800,
-            question: "This word describes someone who eats only plants",
-            answer: "What is vegetarian (or vegan)?",
+            question: "In this type of lawsuit, legal action is filed against a defendant on behalf of a group of people",
+            answer: "What is a class action lawsuit?",
             revealed: false,
           },
           {
             points: 1000,
-            question: "This means having the power to see into the future",
-            answer: "What is visionary?",
-            revealed: false,
-          },
-        ],
-        [
-          {
-            points: 200,
-            question: "Romeo's beloved in Shakespeare's tragic play",
-            answer: "Who is Juliet?",
-            revealed: false,
-          },
-          {
-            points: 400,
-            question:
-              "Elizabeth Bennet's eventual husband in 'Pride and Prejudice'",
-            answer: "Who is Mr. Darcy?",
-            revealed: false,
-          },
-          {
-            points: 600,
-            question: "Scarlett O'Hara's true love in 'Gone with the Wind'",
-            answer: "Who is Rhett Butler?",
-            revealed: false,
-          },
-          {
-            points: 800,
-            question:
-              "This woman drives Gatsby's obsession in Fitzgerald's novel",
-            answer: "Who is Daisy Buchanan?",
-            revealed: false,
-          },
-          {
-            points: 1000,
-            question:
-              "Odysseus's faithful wife who waits 20 years for his return",
-            answer: "Who is Penelope?",
+            question: "They include Percival, Gareth & Galahad",
+            answer: "What are the Knights of the Round Table?",
             revealed: false,
           },
         ],
