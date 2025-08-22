23451aa8f754c67840e201843467e05e5897ef7b
Suharsh Nagasampagi <suharsh329@gmail.com>
2025-08-23 00:45:56 +1000

    Create shreya.js

---
+++ shreya.js
@@ -0,0 +1,182 @@
+const defaultGameData = {
+  boards: [
+    {
+      categories: [
+        "FIRST",
+        "DELULU",
+        "FAVOURITE",
+        "SHREYA THINGS",
+        "MILESTONE"
+      ],
+      questions: [
+        [
+          {
+            points: 200,
+            question: "Who was my first crush?",
+            answer: "Shawn Fernandes ",
+            revealed: false,
+          },
+{
+            points: 400,
+            question: "What was my first car?",
+            answer: "Kia Picanto",
+            revealed: false,
+          },
+{
+            points: 600,
+            question: "When was the first time I came to Australia for Uni?",
+            answer: "Feb 2019",
+            revealed: false,
+          },
+{
+            points: 800,
+            question: "Who was my first pet?",
+            answer: "Billu",
+            revealed: false,
+          },
+{
+            points: 1000,
+            question: "What was my first pair of mainstream shoes?",
+            answer: "Air Force Ones",
+            revealed: false,
+          }
+        ],
+        [
+{
+            points: 200,
+            question: "Also known as telepathy",
+            answer: "What is mind reading?",
+            revealed: false,
+          },
+{
+            points: 400,
+            question: "I wanted to become this before I pursued Chemical Enginenering?",
+            answer: "Doctor",
+            revealed: false,
+          },
+{
+            points: 600,
+            question: "I want this artist to come to my wedding",
+            answer: "Who is Lauv?",
+            revealed: false,
+          },
+{
+            points: 800,
+            question: "I want a house in this suburb",
+            answer: "What is Vaucluse?",
+            revealed: false,
+          },
+{
+            points: 1000,
+            question: "A thumbs up signifies being upset",
+            answer: "What is Shreya's communication skills?",
+            revealed: false,
+          }
+        ],
+        [
+{
+            points: 200,
+            question: "What is my favourite colour?",
+            answer: "Yellow",
+            revealed: false,
+          },
+{
+            points: 400,
+            question: "What is my favourite fruit?",
+            answer: "Mango",
+            revealed: false,
+          },
+{
+            points: 600,
+            question: "What is my favourite Chinese street food?",
+            answer: "Malatang",
+            revealed: false,
+          },
+{
+            points: 800,
+            question: "What is my favourite sport?",
+            answer: "Basketball",
+            revealed: false,
+          },
+{
+            points: 1000,
+            question: "What is my favourite spot in Sydney?",
+            answer: "Sydney Park",
+            revealed: false,
+          }
+        ],
+        [
+{
+            points: 200,
+            question: "Sometimes I put these on my head and ask where did I keep them",
+            answer: "Glasses",
+            revealed: false,
+          },
+{
+            points: 400,
+            question: "A biological cycle every month",
+            answer: "Hot water bag",
+            revealed: false,
+          },
+{
+            points: 600,
+            question: "Morning drink",
+            answer: "Jeera Water",
+            revealed: false,
+          },
+{
+            points: 800,
+            question: "How do I greet my mom when she calls me?",
+            answer: "Amma, heli",
+            revealed: false,
+          },
+{
+            points: 1000,
+            question: "My father wants to hit me with this",
+            answer: "Chapli (slipper)",
+            revealed: false,
+          }
+        ],
+        [
+{
+            points: 200,
+            question: "When did I graduate from UNSW?",
+            answer: "2021",
+            revealed: false,
+          },
+{
+            points: 400,
+            question: "When is my citizenship exam?",
+            answer: "28th Aug",
+            revealed: false,
+          },
+{
+            points: 600,
+            question: "When did I get my PR",
+            answer: "December 2023",
+            revealed: false,
+          },
+{
+            points: 800,
+            question: "At whatr age I learn how to drive?",
+            answer: "22",
+            revealed: false,
+          },
+{
+            points: 1000,
+            question: "Where did I win my first badminton tournament?",
+            answer: "China",
+            revealed: false,
+          }
+        ]
+      ],
+    },
+  ],
+  defaultPlayers: [
+    { name: "Player 1", score: 0 },
+    { name: "Player 2", score: 0 },
+  ],
+};
+
+// Export the game data
+export { defaultGameData };
